7737e4ab96d888d974e4775160b5ccd5db725bc3
Alex Li <lialex0218@gmail.com>
2021-06-24 04:19:21 +0000

    updates parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,3 +1,4 @@
+  
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -10,22 +11,16 @@ const pool = new Pool({
 let which_month = process.argv[2];
 let limit2 = process.argv[3];
 pool.query(`
-
 SELECT DISTINCT (teachers.name) as teacher,  cohorts.name as cohorts
 FROM teachers 
 JOIn assistance_requests ON assistance_requests.teacher_id = teachers.id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher
-`)
+`, [which_month])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohorts}: ${user.teacher}`);
   })
 }).catch(err => console.error('query error', err.stack));
-
-
-
-
-
